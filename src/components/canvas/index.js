041615c68f173268931ad23b718d8d504755a718
Can Sahin <78328974+Cnashn@users.noreply.github.com>
2025-12-24 13:35:39 -0500

    Refactor canvas index to remove ComputersCanvas

--- src/components/canvas/index.js
+++ src/components/canvas/index.js
@@ -1,9 +1,9 @@
 
 import BallCanvas from "./Ball";
-import ComputersCanvas from "./Computers";
 import AnnCanvas from "./Ann";
 
 
 
 
-export { AnnCanvas ,BallCanvas,ComputersCanvas };
+export { AnnCanvas ,BallCanvas};
+
